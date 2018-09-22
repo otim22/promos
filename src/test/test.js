be2af406e24efd32ddbe8bdf06ee994cdd9e8244
Fredrick Otim <fredrickot@gmail.com>
2018-09-22 16:44:09 +0300

    test installation and setup

---
+++ src/test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+require('dotenv').load();
+
+describe('sample test', function() {
+  it('is simple test', function() {
+    console.log('hello');
+  });
+});
