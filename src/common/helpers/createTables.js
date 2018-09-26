035df4dd11a456ac29963b99e357439a50ca3e66
Fredrick Otim <fredrickot@gmail.com>
2018-09-26 17:31:30 +0300

    update createTables

--- src/common/helpers/createTables.js
+++ src/common/helpers/createTables.js
@@ -159,7 +159,6 @@ module.exports = {
         },
       },
     };
-    
     /* eslint-disable max-len */
     promosdb.discoverModelProperties('promotions', function(err, props) {
       if (Object.keys(props).length === 0) {
@@ -172,6 +171,5 @@ module.exports = {
       }
     });
     /* eslint-enable max-len */
-  },
-
+  }
 };
